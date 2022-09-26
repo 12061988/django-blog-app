3bf347bf6fafdfbdc5d656f07b89ed67186cbcef
12061988 <m.maguettefall@gmail.com>
2022-09-26 13:14:32 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ["DATABASE_URL"]= "postgres://brzmhzhwfmapfu:4a0441f8e07ae4d9fef4ad200c707c317ad164b9748417fc978fbf255c63f005@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dalnvahrvaslcs"
+os.environ["SECRET_KEY"]= "lesfleursdumal1857"
+os.environ["CLOUDINARY_URL"] = "cloudinary://233728459262718:EKOoDLnSLPjo8jGsqcsMjYo4_pg@diqcnssbg"
